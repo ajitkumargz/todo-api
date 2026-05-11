ba2eaa0723082d323b87be0a4157daca487fa417
Ajit Kumar <ajitkumargz19@gmail.com>
2026-05-12 01:23:36 +0530

    CrossOrigin corrected

--- src/main/java/com/todoapp/Controller/TaskController.java
+++ src/main/java/com/todoapp/Controller/TaskController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/todo")
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "https://todo-ui-weld-kappa.vercel.app/")
 public class TaskController {
 
     private TaskService taskService;
